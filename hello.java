dddf396d1f676c5af7f42c3aaa637dfca70a94b7
笨小孩公元1998 <45307677+StupidKid1998@users.noreply.github.com>
2020-02-27 22:25:11 +0800

    创建Java文件
    第一次使用GitHub创建项目文件

---
+++ hello.java
@@ -0,0 +1,23 @@
+package com.yan.dao;
+
+import com.yan.pojo.Books;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface BooksMapper {
+//      查看所有的书
+      List<Books> showAll();
+//      通过id查找书
+      Books showBookById(@Param("bookID") int bookID);
+//      添加一本书
+      int addBook(Books books);
+//      修改一本书
+      int updateBook(Books books);
+//      删除一本书
+      int deleteBook(@Param("bookID") int bookID);
+//      通过书名查询
+      Books showBookByName(@Param("bookName") String bookName);
+//      模糊查询
+      List<Books> showBookByLike(@Param("bookName") String bookName);
+}
